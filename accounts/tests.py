f1b443278a9a7f9854936a254315d9725cc4a247
Agassem Sanda Florentin <agassemflorentin@gmail.com>
2026-04-17 17:52:18 +0100

    setup: starting project app

--- accounts/tests.py
+++ accounts/tests.py
@@ -30,3 +30,60 @@ class UserInscriptionTest(APITestCase):
         self.assertTrue(
             User.objects.filter(email="test@test.com").exists()
         )
+
+    # test avec password et confirm_password differents
+    def test_password_mismatch(self):
+        data = {
+            "email": "test@test.com",
+            "password": "test123!",
+            "confirm_password": "test12",
+            "regle_confidentialite": True,
+        }
+
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("password", response.data)
+
+    # tester une creation sans email
+    def test_missing_email(self):
+        data = {
+            "password": "test123",
+            "confirm_password": "test123",
+            "regle_confidentialite": True,
+        }
+
+        response = self.client.post(self.url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("email", response.data)
+
+    # creation avec un email existant
+    def test_existing_email(self):
+        User.objects.create_user(
+            email= "test@test.com",
+            password= "test126",
+            regle_confidentialite= True,
+        )
+
+        data = {
+            "email": "test@test.com",
+            "password": "test123!",
+            "confirm_password": "test12",
+            "regle_confidentialite": True,
+        }
+
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    # creation sans regle de confidentialite
+    def test_without_confidentialite_rule(self):
+        data = {
+            "email": "test@test.com",
+            "password": "test123!",
+            "confirm_password": "test12",
+        }
+
+        response = self.client.post(self.url, data)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertIn("regle_confidentialite", response.data)
